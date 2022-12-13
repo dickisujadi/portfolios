7ab271f5ac3dffdfb1ac462cd2568757cf4c78e6
dickisujadi <dicki.sujadi@eaciit.com>
2022-12-13 17:34:28 +0700

    implement scroll effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
 interface HeaderProps {
     content: string,
     onClick?: () => void
 }
 
 export default function Header({ content, onClick }: HeaderProps) {
-    return <div className="fixed w-screen min-h-[10vh] border-b bg-[var(--black-100)] z-20">
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        const scrollHandler = () => {
+        if(window.scrollY > 0) {
+            setIsScrolled(true)
+        } else {
+            setIsScrolled(false)
+        }
+        }
+
+        window.addEventListener("scroll", scrollHandler)
+
+        return () => {
+        window.removeEventListener('scroll', scrollHandler)
+        }
+    }, []);
+
+    
+    return <div className={`fixed w-screen min-h-[10vh] ${isScrolled && 'shadow-xl'} bg-[var(--black-100)] z-20`}>
         <p className="capitalize font-normal text-4xl text-left ml-[7vw] mt-[2vh] xs:text-2xl"><span className="hover:cursor-pointer" onClick={onClick}>{`< `}</span>{content}</p>
     </div>
 }
