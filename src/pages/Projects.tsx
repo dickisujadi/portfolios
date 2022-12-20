f884d17047571feca117575c8e10c3b5c079eb58
dickisujadi <dicki.sujadi@eaciit.com>
2022-12-20 22:39:16 +0700

    set header and projects page on sm and xs

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,32 +1,52 @@
 import { useState } from "react";
 import { experiences } from '../contents/Experiences';
 import { Experience } from "../typings/Interfaces";
+import { GrFormClose } from "react-icons/gr";
+import { AiFillCaretDown } from "react-icons/ai";
 
 export default function Projects() {
     const [experience, setExperience] = useState<Experience>(experiences[0]);
+    const [showMenu, setShowMenu] = useState(true)
 
     const changeExperience = (newExperience: Experience) => {
         setExperience(newExperience);
     }
 
-    return <div className="flex flex-row w-screen mx-[10vw] py-[15vh]">
-        <div className="max-w-[10vw] flex flex-col h-screen">
-            {experiences.map((experience: Experience, index:number) => {
-                return <img key={index} src={experience.logo} alt={`${experience.id}`} className="w-[10vh] py-[2vh] hover:cursor-pointer" onClick={() => changeExperience(experience)}/>
+    const setMenuHandler = () => {
+        console.log('clicked');
+        
+        setShowMenu(!showMenu);
+    }
+
+    return <div className="flex flex-row w-screen mx-[10vw] py-[13vh] xs:flex-col">
+        <div className="max-w-[10vw] flex flex-col h-fit xs:hidden">
+            {experiences.map((experience_: Experience, index:number) => {
+                return <img key={index} src={experience_.logo} alt={`${experience_.id}`} className="w-[10vw] py-[2vh] hover:cursor-pointer" onClick={() => changeExperience(experience_)}/>
             })}
         </div>
-        <div className="max-w-[70vw] mx-[5vw] text-left">
-            <p className={`${experience.color} font-['Roboto'] text-2xl`}>{experience.company}</p>
-            <p className={`text-4xl`}>{experience.position}</p>
+        {showMenu ? <div className="hidden xs:block xs:fixed flex-col mx-[5vw] text-left mb-[3vh] bg-white w-screen pb-[2vh]">
+            <GrFormClose className="hover:cursor-pointer" onClick={setMenuHandler}/>
+            {experiences.map((experience_: Experience, index:number) => {
+                return <p className={`hover:cursor-pointer my-[.5vh] ${experience_.id == experience.id && 'text-[var(--primary-blue)]'}`} key={index} onClick={() => changeExperience(experience_)}>
+                    {experience_.company}
+                </p>
+            })}
+        </div>:
+        <div className="xs:flex flex-row hidden py-[1vh] mx-[5vw] hover:cursor-pointer fixed bg-white w-screen" onClick={setMenuHandler}>{experience.company} <AiFillCaretDown /></div>
+        }
+
+        <div className="max-w-[70vw] mx-[5vw] text-left pt-[10vh]">
+            <p className={`${experience.color} font-['Roboto'] text-2xl sm:text-xl xs:text-lg`}>{experience.company}</p>
+            <p className="text-4xl sm:text-3xl xs:text-2xl">{experience.position}</p>
             <div className="my-8 text-primary-grey">
-                <p className={`text-xl`}>{experience.period}</p>
-                <p className={`text-xl`}>{experience.country}</p>
-                <p className={`text-xl`}>{experience.industry}</p>
+                <p className="text-xl sm:text-lg xs:text-base">{experience.period}</p>
+                <p className="text-xl sm:text-lg xs:text-base">{experience.country}</p>
+                <p className="text-xl sm:text-lg xs:text-base">{experience.industry}</p>
             </div>
             <div className="">
-                <p className="text-2xl font-['Roboto'] flex-wrap my-8">{experience.jobDescHeader}</p>
+                <p className="text-2xl sm:text-xl xs:text-lg font-['Roboto'] flex-wrap my-8">{experience.jobDescHeader}</p>
                 {experience.jobDesc.map((jobDesc: string, index: number) => {
-                    return <p className="text-2xl font-['Roboto'] flex-wrap my-4" key={index}>{jobDesc}</p>
+                    return <p className="text-2xl sm:text-xl xs:text-lg font-['Roboto'] flex-wrap my-4" key={index}>{jobDesc}</p>
                 })}
             </div>
         </div>
--- src/ui/molecules/Header.tsx
+++ src/ui/molecules/Header.tsx
@@ -51,9 +51,9 @@ export default function Header({ content, onClick }: HeaderProps) {
 
     
     return <>
-        <div className={`fixed w-screen min-h-[10vh] ${isMenuAppears&& 'min-h-[15vh]'} ${isScrolled && 'shadow-xl'} bg-white z-20 flex flex-row items-center justify-center xs:flex-col`}>
+        <div className={`fixed w-screen min-h-[10vh] xs:h-[13vh] ${isScrolled && 'shadow-xl xs:shadow-none'} bg-white z-20 flex flex-row items-center justify-center xs:flex-col`}>
             <div className={`xs:block hidden ${isMenuAppears&& 'w-screen px-[30vw]'}`} onClick={setMenuHandler}>
-                {isMenuAppears? <GrFormClose /> : <TbMenu />}
+                {isMenuAppears? <GrFormClose className="hover:cursor-pointer"/> : <TbMenu />}
             </div>
             <div className={`${!isMenuAppears ? 'xs:hidden flex' : 'xs:flex xs:flex-col flex'}`}>
                 {contents.map(content => {
