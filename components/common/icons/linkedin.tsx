164b0dee81185daf247f004d49966aa1f50c4016
dickisujadi <dickisujadi@gmail.com>
2023-07-22 09:54:10 +0700

    fix projects

---
+++ components/common/icons/linkedin.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+type Props = {
+  onClick: () => void;
+};
+
+export default function LinkedInIcon({ onClick }: Props) {
+  return (
+    <div className={`flex flex-col items-center justify-center`}>
+      <Image
+        onClick={onClick}
+        src="/assets/linkedin.svg"
+        width="250"
+        height="113"
+        className="h-[4vh] w-[4vw] cursor-pointer"
+        alt="empty state"
+        priority
+        sizes="250px"
+      />
+    </div>
+  );
+}
--- components/projects.tsx
+++ components/projects.tsx
@@ -4,6 +4,7 @@ import {
   montserrat_300,
   montserrat_400,
 } from "@/common/fonts";
+import LinkedInIcon from "./common/icons/linkedin";
 
 export default function Projects() {
   return (
@@ -16,43 +17,32 @@ export default function Projects() {
           Senior Front End Engineer
         </p>
         <p className={`${montserrat_300.className} text-xl sm:text-base`}>
-          I have delivered:
+          I have delivered
         </p>
         <p
           className={`${montserrat_300.className} text-xl my-6 mx-[10vw] sm:text-base sm:mx-0`}
         >
-          <span className={`text-rose-700`}>Customer Care Application</span> is
-          designed to facilitate communication between Customer and Agents
-          (Employees) through chat, voice calls, or video calls
+          <span className={`text-rose-700`}>Customer Care Application</span>,
+          that is designed to facilitate communication between Customer and
+          Agents (Employees) through chat, voice calls, or video calls.
         </p>
         <p
-          className={`${montserrat_300.className} text-xl mx-[10vw] sm:text-base sm:mx-0`}
+          className={`${montserrat_300.className} text-xl mx-[10vw] my-6 sm:text-base sm:mx-0`}
         >
-          <span className={`text-rose-700`}>SSO Application</span>, application
-          that secured Authentication and Authorization of {"clients'"}{" "}
-          Application
-        </p>
-      </div>
-      <div className="flex flex-col w-3/5 mx-auto h-screen justify-center items-center text-center sm:w-5/6">
-        <p className={`${montserrat_400.className} text-3xl sm:text-xl`}>
-          Eaciit
-        </p>
-        <p className={`${montserrat_200.className} text-5xl mb-20 sm:text-3xl`}>
-          Front End Engineer
-        </p>
-        <p className={`${montserrat_300.className} text-xl sm:text-base`}>
-          I have delivered:
+          <span className={`text-rose-700`}>SSO Application</span>, is an
+          application that ensures the secure authentication and authorization
+          of {"clients'"} applications.
         </p>
         <p
-          className={`${montserrat_300.className} text-xl my-6 mx-[10vw] sm:text-base sm:mx-0`}
+          className={`${montserrat_300.className} text-xl mt-28 mb-6 sm:text-base`}
         >
-          <span className={`text-orange-700`}>
-            Cashflow Management Applications
-          </span>{" "}
-          serve the purpose of overseeing and enhancing key performance
-          indicators (KPIs) to achieve greater efficiencies and boost the
-          generation of free cash flow.
+          See more on my LinkedIn
         </p>
+        <LinkedInIcon
+          onClick={() =>
+            window.open("https://www.linkedin.com/in/dickisujadi/", "_blank")
+          }
+        />
       </div>
     </>
   );
