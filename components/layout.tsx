9fed2c7e8bb33cc22bc9713fe0de4063937d558c
dickisujadi <dickisujadi@gmail.com>
2023-11-21 09:37:19 +0700

    add website analytics and components

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Background from "./background";
 import "../styles.css";
+import { GoogleAnalyticsTracking } from "@/trackers/google";
 
 type Props = {
   children: ReactNode;
@@ -9,6 +10,8 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <>
+      <GoogleAnalyticsTracking />
+
       <main className="text-white">
         <Background />
         {children}
